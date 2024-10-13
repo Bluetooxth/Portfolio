ca6d66c05ca825a1db7ec1071da0f85bca40e0f0
Priyanshu Chahar <165533860+Bluetooxth@users.noreply.github.com>
2024-10-13 12:46:24 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -39,7 +39,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={font.className}
       >
         {children}
         <Footer />
