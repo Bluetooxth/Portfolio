db7ebbbcede695d3f5dc5375a286f1003597fe25
Priyanshu Chahar <priyannxhuu@gmail.com>
2024-10-30 21:07:05 +0530

    added skills

--- data/Skills.js
+++ data/Skills.js
@@ -1,96 +1,111 @@
 import {
-    FaJsSquare,
-    FaReact,
-    FaNodeJs,
-    FaGitAlt,
-    FaGithub,
-    FaLinux,
-  } from "react-icons/fa";
-  import {
-    SiTailwindcss,
-    SiTypescript,
-    SiNextdotjs,
-    SiExpress,
-    SiMongodb,
-    SiMongoose,
-    SiPython,
-    SiRust,
-    SiPostman,
-  } from "react-icons/si";
-  import { RiRemixRunFill } from "react-icons/ri";
-  import { FaGolang } from "react-icons/fa6";
-  import { TbBrandPrisma } from "react-icons/tb";
-  import { SiPostgresql } from "react-icons/si";
-  
-  const skills = [
-    {
-      skillName: "JavaScript",
-      skillIcon: FaJsSquare,
-    },
-    {
-      skillName: "TypeScript",
-      skillIcon: SiTypescript,
-    },
-    {
-      skillName: "TailwindCSS",
-      skillIcon: SiTailwindcss,
-    },
-    {
-      skillName: "ReactJS",
-      skillIcon: FaReact,
-    },
-    {
-      skillName: "NextJS",
-      skillIcon: SiNextdotjs,
-    },
-    {
-      skillName: "NodeJS",
-      skillIcon: FaNodeJs,
-    },
-    {
-      skillName: "ExpressJS",
-      skillIcon: SiExpress,
-    },
-    {
-      skillName: "Mongoose",
-      skillIcon: SiMongoose,
-    },
-    {
-      skillName: "Prisma",
-      skillIcon: TbBrandPrisma,
-    },
-    {
-      skillName: "MongoDB",
-      skillIcon: SiMongodb,
-    },
-    {
-      skillName: "PostgreSQL",
-      skillIcon: SiPostgresql,
-    },
-    {
-      skillName: "Golang",
-      skillIcon: FaGolang,
-    },
-    {
-      skillName: "Python",
-      skillIcon: SiPython,
-    },
-    {
-      skillName: "Git",
-      skillIcon: FaGitAlt,
-    },
-    {
-      skillName: "Github",
-      skillIcon: FaGithub,
-    },
-    {
-      skillName: "Postman",
-      skillIcon: SiPostman,
-    },
-    {
-      skillName: "Linux",
-      skillIcon: FaLinux,
-    },
-  ];
-  
-  export default skills;
+  FaJsSquare,
+  FaReact,
+  FaNodeJs,
+  FaGitAlt,
+  FaGithub,
+  FaLinux,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiTypescript,
+  SiNextdotjs,
+  SiExpress,
+  SiMongodb,
+  SiMongoose,
+  SiPython,
+  SiRust,
+  SiPostman,
+} from "react-icons/si";
+import { RiRemixRunFill } from "react-icons/ri";
+import { FaGolang } from "react-icons/fa6";
+import { TbBrandPrisma } from "react-icons/tb";
+import { SiPostgresql } from "react-icons/si";
+import { GiBearFace } from "react-icons/gi";
+import { TbBrandReactNative } from "react-icons/tb";
+import { SiExpo } from "react-icons/si";
+
+const skills = [
+  {
+    skillName: "JavaScript",
+    skillIcon: FaJsSquare,
+  },
+  // {
+  //   skillName: "TypeScript",
+  //   skillIcon: SiTypescript,
+  // },
+  {
+    skillName: "TailwindCSS",
+    skillIcon: SiTailwindcss,
+  },
+  {
+    skillName: "React Native",
+    skillIcon: TbBrandReactNative,
+  },
+  {
+    skillName: "Expo",
+    skillIcon: SiExpo,
+  },
+  {
+    skillName: "ReactJS",
+    skillIcon: FaReact,
+  },
+  {
+    skillName: "NextJS",
+    skillIcon: SiNextdotjs,
+  },
+  {
+    skillName: "NodeJS",
+    skillIcon: FaNodeJs,
+  },
+  {
+    skillName: "ExpressJS",
+    skillIcon: SiExpress,
+  },
+  {
+    skillName: "Mongoose",
+    skillIcon: SiMongoose,
+  },
+  {
+    skillName: "Prisma",
+    skillIcon: TbBrandPrisma,
+  },
+  {
+    skillName: "MongoDB",
+    skillIcon: SiMongodb,
+  },
+  {
+    skillName: "PostgreSQL",
+    skillIcon: SiPostgresql,
+  },
+  {
+    skillName: "Zustand",
+    skillIcon: GiBearFace,
+  },
+  {
+    skillName: "Golang",
+    skillIcon: FaGolang,
+  },
+  {
+    skillName: "Python",
+    skillIcon: SiPython,
+  },
+  {
+    skillName: "Git",
+    skillIcon: FaGitAlt,
+  },
+  {
+    skillName: "Github",
+    skillIcon: FaGithub,
+  },
+  {
+    skillName: "Postman",
+    skillIcon: SiPostman,
+  },
+  {
+    skillName: "Linux",
+    skillIcon: FaLinux,
+  },
+];
+
+export default skills;
